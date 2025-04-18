96096bbded890924050dd444f4b1fa42d689c7f2
Richard Jesudason <richard@jesudason.co.uk>
2025-04-18 07:48:48 +0100

    test: Installed istanbul

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,16 @@ import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config';
 
 export default defineWorkersConfig({
 	test: {
+		coverage: {
+			provider: 'istanbul',
+			reporter: ['text', 'html'],
+			exclude: [
+				'scripts/**',
+				'test/**', // ✅ optional: ignore tests themselves
+				'**/*.test.*', // ✅ optional: ignore test files
+				'vitest.config.*', // ✅ optional: ignore config
+			],
+		},
 		poolOptions: {
 			workers: {
 				wrangler: { configPath: './wrangler.jsonc' },
